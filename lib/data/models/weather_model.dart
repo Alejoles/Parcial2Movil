a79c4e3429cbcfa2e241a076601bda47814e401b
Alejoles <rulitosxd1@gmail.com>
2021-11-10 19:42:09 -0500

    model ready

--- lib/data/models/weather_model.dart
+++ lib/data/models/weather_model.dart
@@ -20,5 +20,7 @@ class WeatherItem {
     mainFeelsLike = json['main']['feels_like'];
     mainHumidity = json['main']['humidity'];
     windSpeed = json['wind']['speed'];
+    ciudad = json['name'];
+    pais = json['sys']['country'];
   }
 }
